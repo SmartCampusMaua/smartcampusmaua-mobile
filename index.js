a17b9dd2d4763529f2332abe1c98cdf85d83a10f
Rogério Cassares <46907805+roger10cassares@users.noreply.github.com>
2022-05-26 11:51:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  */
 
 import { AppRegistry } from 'react-native';
-// import { RootScreen } from './src/pages/RootScreen';
 import App from './src/pages/RootStackScreen';
 import { name as appName } from './app.json';
 
